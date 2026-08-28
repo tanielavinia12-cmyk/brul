10e1a051575bfe97b61c84353934f3a8480480ec
Bruno Henrique Nogueira Soares De Oliveira <00001092812830sp@aluno.educacao.sp.gov.br>
2026-08-28 19:16:10 -0300

    fase 3

--- script.js
+++ script.js
@@ -7,3 +7,22 @@ let curso = "Informática";
 console.log(nome);
 console.log(idade);
 console.log(curso);
+
+
+let alunos = ["João", "Maria", "Pedro", "bruno", "lavinia"];
+
+console.log(alunos[0]);
+console.log(alunos[1]);
+console.log(alunos[2]);
+console.log(alunos[3]);
+console.log(alunos[4]);
+
+for (let i = 0; i < alunos.length; i++) {
+    console.log(alunos[i]);
+}
+
+let lista = document.getElementById("listaAlunos");
+
+for (let i = 0; i < alunos.length; i++) {
+    lista.innerHTML += `<li>${alunos[i]}</li>`;
+}
